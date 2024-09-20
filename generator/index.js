1f40e1d04699bead40106f983f9940aeb7cc52fc
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2024-09-20 16:41:56 +0200

    ejs files deleted after resolved

--- generator/index.js
+++ generator/index.js
@@ -13,44 +13,38 @@ function copyDirectory(src, dest, auth) {
   let entries = fs.readdirSync(src, { withFileTypes: true });
   const appName = path.basename(process.cwd()); // Get the app name
 
-
   entries.forEach(entry => {
     let srcPath = path.join(src, entry.name);
     let destPath = path.join(dest, entry.name);
 
+    if (entry.name === '.gitlab-ci.ejs' || entry.name === 'catalog-info.ejs') {
+      const template = fs.readFileSync(srcPath, 'utf8');
+      const renderedTemplate = ejs.render(template, { appName });
 
-  if (entry.name === '.gitlab-ci.ejs' || entry.name === 'catalog-info.ejs') {
-  const template = fs.readFileSync(srcPath, 'utf8');
-  const renderedTemplate = ejs.render(template, { appName });
+      // Set the output filename and directory for the resolved EJS files
+      const baseName = path.basename(entry.name, '.ejs');
+      const targetPath = path.join(process.cwd(), `${baseName}.yaml`);
 
-  // Set the output filename and directory for the resolved EJS files
-  const baseName = path.basename(entry.name, '.ejs');
-  const targetPath = path.join(process.cwd(), `${baseName}.yaml`);
+      // Ensure the directory exists before writing the file
+      mkdirp.sync(path.dirname(targetPath));
 
-  fs.writeFileSync(targetPath, renderedTemplate);
-} else {
-  if (entry.isDirectory()) {
-    copyDirectory(srcPath, destPath, auth);
-  } else if (path.extname(entry.name).toLowerCase() !== '.ejs' || entry.name.startsWith('.')) {
-    fs.copyFileSync(srcPath, destPath);
-  }
-}
+      fs.writeFileSync(targetPath, renderedTemplate);
+      return;  // Prevent further processing for these files
+    }
 
-
-    if(auth === "No Authentication") {
-
-      if(entry.name === 'craco.config.js') { 
-        return;
-      }
+    if (auth === "No Authentication" && entry.name === 'craco.config.js') {
+      return;
     }
 
     if (entry.isDirectory()) {
-      copyDirectory(srcPath, destPath);
-    } else if (path.extname(entry.name).toLowerCase() !== '.ejs' || entry.name.startsWith('.') ) {
+      copyDirectory(srcPath, destPath, auth);
+    } else if (path.extname(entry.name).toLowerCase() !== '.ejs') {
+      // Change condition to exclude .ejs files explicitly
       fs.copyFileSync(srcPath, destPath);
     }
   });
 }
+
 
 inquirer.prompt(prompts).then((promptResults) => {
   consoleLogger(promptResults);
@@ -160,118 +154,3 @@ function processEjsTemplates(srcComponentsPath, promptResults) {
     }
   }
 }
-
-
-//NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
-
-
-
-// function processEjsTemplates(srcPath, promptResults, isYaml = false) {
-//   const entries = fs.readdirSync(srcPath, { withFileTypes: true });
-//   const appName = path.basename(process.cwd()); // Get the app name
-
-//   for (let entry of entries) {
-//     const fullPath = path.join(srcPath, entry.name);
-
-//     if (entry.isDirectory()) {
-//       // Recursively process subdirectories
-//       processEjsTemplates(fullPath, promptResults, isYaml);
-//     } else if (path.extname(entry.name).toLowerCase() === '.ejs') {
-//       const template = fs.readFileSync(fullPath, 'utf8');
-//       const renderedTemplate = ejs.render(template, { promptResults, appName });
-
-//       // Determine the output extension and directory based on the input parameter
-//       const outputExtension = isYaml ? '.yaml' : '.jsx';
-//       const baseName = path.basename(entry.name, '.ejs');
-
-//       // Check if it's the catalog-info file
-//       if (isYaml && baseName === 'catalog-info') {
-//         const targetPath = path.join(process.cwd(), `${baseName}${outputExtension}`);
-//         fs.writeFileSync(targetPath, renderedTemplate);
-//       } else {
-//         const targetDir = isYaml ? path.join(process.cwd(), 'templates') : path.join(process.cwd(), 'src/components', baseName);
-//         const targetPath = path.join(targetDir, `${baseName}${outputExtension}`);
-
-//         mkdirp.sync(targetDir);
-//         fs.writeFileSync(targetPath, renderedTemplate);
-//       }
-//     }
-//   }
-// }
-
-// inquirer.prompt(prompts).then((promptResults) => {
-//   consoleLogger(promptResults);
-//   const configString = `export default ${JSON.stringify(promptResults)};`;
-//   const configPath = path.join(process.cwd(), 'src', 'config.js');
-//   fs.writeFileSync(configPath, configString);
-
-//   let dependencies = {
-//     "@infineon/infineon-design-system-react": "^23.4.2",
-//     "react": "^18.3.1",
-//     "react-dom": "^18.3.1",
-//     "react-scripts": "5.0.1",
-//     "react-router": "^6.23.1",
-//     "react-router-dom": "^6.23.1",
-//     "sass": "^1.77.6",
-//   };
-
-//   let devDependencies = {
-//     "@babel/core": "^7.23.5",
-//     "@babel/eslint-parser": "^7.23.3",
-//     "@babel/preset-react": "^7.24.1",
-//     "eslint": "^8.0.0",
-//     "eslint-config-react-app": "^7.0.1",
-//     "eslint-plugin-react": "^7.34.1"
-//   };
-
-//   if(promptResults.authRequired !== "No Authentication") {
-//     dependencies["@miami/miami"] = "^1.1.0";
-//     dependencies["craco"] = "^0.0.3";
-//     devDependencies["axios"] = "^1.6.4";
-//     devDependencies["react-toastify"] = "^10.0.5";
-//     devDependencies["concurrently"]= "^8.2.2";
-//   }
-
-//   const projectPackageJsonPath = path.resolve(process.cwd(), 'package.json'); 
-//   const projectPackageJson = JSON.parse(fs.readFileSync(projectPackageJsonPath, 'utf8'));
-  
-//   projectPackageJson.dependencies = { ...projectPackageJson.dependencies, ...dependencies };
-//   projectPackageJson.devDependencies = { ...projectPackageJson.devDependencies, ...devDependencies };
-
-//   const additionalScripts = {
-//     start: promptResults.authRequired !== "No Authentication" ? "concurrently \"npm run start:dev\" \"npm run start:serve\"" : "react-scripts start",
-//     build: promptResults.authRequired !== "No Authentication" ? "craco build" : "react-scripts build",
-//     "test:e2e": "playwright test",
-//     lint: "eslint --ext .jsx,.js,.cjs,.mjs --fix --ignore-path .gitignore",
-//     format: "prettier --write src/",
-//   };
-
-//   if (promptResults.authRequired !== "No Authentication") {
-//     additionalScripts["start:dev"] = "set PORT=8080 && craco start";
-//     additionalScripts["start:serve"] = "docker compose up || echo 'Docker command failed. Please make sure you have Docker installed.'";
-//   }
-
-//   projectPackageJson.scripts = { ...projectPackageJson.scripts, ...additionalScripts };
-
-//   fs.writeFileSync(projectPackageJsonPath, JSON.stringify(projectPackageJson, null, 2));
-
-//   const templatesSrcPath = path.resolve(__dirname, './templates/src');
-//   const projectSrcPath = path.resolve(process.cwd(), 'src');
-//   copyDirectory(templatesSrcPath, projectSrcPath);
-
-//   const templateFilesPath = path.resolve(__dirname, './templateFiles');
-//   const projectRootPath = process.cwd();
-//   copyDirectory(templateFilesPath, projectRootPath);
-
-//   const templatesPath = path.resolve(__dirname, './templates');
-//   copyDirectory(templatesPath, projectRootPath, promptResults.authRequired);
-
-//   const componentsPath = path.resolve(__dirname, 'templates/src/components');
-//   processEjsTemplates(componentsPath, promptResults, false);
-
-//   // Process the catalog-info.ejs file separately
-//   const catalogInfoPath = path.resolve(__dirname, './templates/catalog-info.ejs');
-//   if (fs.existsSync(catalogInfoPath)) {
-//     processEjsTemplates(templatesPath, promptResults, true);
-//   }
-// });
