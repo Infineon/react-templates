c5b665fdb6f8f9bb0f91d06fdb0c5978a7b06e35
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2024-11-26 09:08:18 +0100

    created table template

--- generator/templates/src/components/Layout/template-01/HomePage/HomePage.jsx
+++ generator/templates/src/components/Layout/template-01/HomePage/HomePage.jsx
@@ -1,38 +1,33 @@
 import Table from '../Table/Table';
-import Sidebar from '../Sidebar/Sidebar';
-import Button from '../Button/Button';
 import Navbar from '../Navbar/Navbar';
-import Card from '../Card/Card';
+import Pagination from '../Pagination/Pagination';
+import data from '../Table/mock-data.json'
 import './homePage.scss';
+import { useState } from 'react';
 
 function HomePage() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage, setPostsPerPage] = useState(10)
+  
+  function handlePageChange(event) {
+    setCurrentPage(event.detail.currentPage)
+    setPostsPerPage(event.detail.itemsPerPage)
+  }
 
   return (
     <div className='container'>
-      <div className="sidebar__wrapper">
-          <Sidebar /> 
+      <div className="navbar__wrapper">
+        <Navbar />
       </div>
-      <div className="right__side-wrapper">
-        <div className="top__wrapper">
-            <div className="navbar__wrapper">
-              <Navbar />
-            </div>
-          <div className="card__wrapper">
-            <Card direction='horizontal' />
-            <h2>Important headline related to the Card</h2>
+      <div className="main__wrapper">
+        <div className="table__wrapper">
+          <div className="table__desc">
+            <h2>Table name</h2>
           </div>
+          <Table currentPage={currentPage} postsPerPage={postsPerPage} />
         </div>
-        <div className="main__content-wrapper">
-          <div className="header__wrapper">
-            <h3 className="title__wrapper">Important Data</h3>
-            <div className="buttons__wrapper">
-              <Button variant='primary' />
-              <Button variant='secondary' />
-            </div>
-          </div>
-          <div className="table__wrapper">
-            <Table />
-          </div>
+        <div className="pagination__wrapper">
+          <Pagination total={data[0].rows.length} currentPage={1} onIfxPageChange={handlePageChange} />
         </div>
       </div>
     </div>
--- generator/templates/src/components/Table/Table.jsx
+++ generator/templates/src/components/Table/Table.jsx
@@ -1,36 +1,19 @@
 import { IfxBasicTable } from '@infineon/infineon-design-system-react';
+import data from './mock-data.json';
 import './table.scss';
 
-function Table() {
+function Table({ currentPage, postsPerPage }) {
 
-  const cols = [
-    {"headerName":"Manufacturer","field":"Manufacturer","sortable":true,"sort":"desc","unSortIcon":true},{"headerName":"Model","field":"model","sortable":true,"unSortIcon":true},
-    {"headerName":"Price","field":"price"},
-    {"headerName":"Age","field":"age"}
-  ];
-
-  const rows = [
-    {"Manufacturer":"Toyota","model":"Prius","price":35000,"age":10},
-    {"Manufacturer":"Ford","model":"Mondeo","price":32000,"age":12},
-    {"Manufacturer":"Porsche","model":"Boxster","price":72000}, 
-    {"Manufacturer":"Suzuki","model":"Vitara","price":35000,"age":10}, 
-    {"Manufacturer":"Honda","model":"Civic","price":35000,"age":10}, 
-    {"Manufacturer":"Nissan","model":"Maxima","price":35000,"age":10}, 
-    {"Manufacturer":"Tesla","model":"S","price":35000,"age":10}, 
-    {"Manufacturer":"Volkswagen","model":"Golf","price":35000,"age":10}, 
-    {"Manufacturer":"Seat","model":"Ibiza","price":35000,"age":10}, 
-    {"Manufacturer":"Lada","model":"Niva","price":35000,"age":10}, 
-    {"Manufacturer":"Dacia","model":"Sandero","price":35000,"age":10}, 
-    {"Manufacturer":"Renault","model":"Captur","price":35000,"age":10}, 
-    {"Manufacturer":"Hyundai","model":"Ioniq 5","price":35000,"age":10}
-  ];
-
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = data[0].rows.slice(indexOfFirstPost, indexOfLastPost);
+  
   return (
     <IfxBasicTable 
-    row-height='default'
-    cols={JSON.stringify(cols)}
-    rows={JSON.stringify(rows)}
-    table-height='auto'>
+      row-height='default'
+      cols={JSON.stringify(data[1].cols)}
+      rows={JSON.stringify(currentPosts)}
+      table-height='auto'>
     </IfxBasicTable>
   );
 }
